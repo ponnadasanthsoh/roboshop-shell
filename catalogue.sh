afb528cf85e3ea505663cd8ad9c020a652eb13ec
ponnadasanthsoh <ponnadasanthosh7@gmail.com>
2024-03-19 21:42:10 +0530

    roboshpo-shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -97,7 +97,7 @@ VALIDATE $? "Installing MongoDB client"
 
 mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> $LOGFILE
 
-VALIDATE $? "Loading catalouge data into MongoDB"
+VALIDATE $? "Loading catalogue data into MongoDB"
 
 
 
